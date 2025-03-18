bd5d196a7a1420355e77ab335c636493c07b248f
Cryaroonie <danilouismanaloto@gmail.com>
2025-03-18 10:28:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    alert("Thank you for Messaging us, we'll get back to you and your order as soon as possible");
+    document.getElementById("contactForm").reset();
+}
